a5e85abf6310d960064d0e17ee57d15a5b253482
hugohal <hhalablian@gmail.com>
2025-02-17 10:11:09 +0100

    Add multi-threading support with thread management and calculations

---
+++ prog_multi_tache/thread/exo1.c
@@ -0,0 +1,64 @@
+#include <string.h>
+#include <stdio.h>//perror
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+struct paramsFonctionThread {
+
+  int idThread;
+
+  // si d'autres paramètres, les ajouter ici.
+
+};
+
+
+void * fonctionThread (void * params){
+
+  struct paramsFonctionThread * args = (struct paramsFonctionThread *) params;
+
+  // a compléter
+  ...
+}
+
+
+int main(int argc, char * argv[]){
+
+  if (argc < 2 ){
+    printf("utilisation: %s  nombre_threads  \n", argv[0]);
+    return 1;
+  }     
+
+  
+  pthread_t threads[atoi(argv[1])];
+
+  ...
+
+ 
+  
+  // création des threards 
+  for (int i = 0; i < atoi(argv[1]); i++){
+
+    // Le passage de paramètre est fortement conseillé (éviter les
+    // variables globles).
+
+     ... // compléter pour initialiser les paramètres
+    if (pthread_create(&threads[i], NULL,..., ...) != 0){
+      perror("erreur creation thread");
+      exit(1);
+    }
+  }
+
+
+// garder cette saisie et modifier le code en temps venu. 
+  printf("saisir un caractère \n");
+  fgetc(stdin);
+
+ ... compléter
+
+  return 0;
+ 
+}
+ 
